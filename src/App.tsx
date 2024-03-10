0ad25a120ee663b283e980d3bb56ff5929342f42
Brandon Tippings <tippingsbrandon@gmail.com>
2024-03-09 22:19:58 -0600

    Added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,46 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header';
+import React, {useState, useCallback, useEffect} from 'react';
+// @ts-ignore
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+function Math() {
+    return <div>Math</div>;
+}
+
+function Programming() {
+    return <div>Programming</div>;
+}
+
+function Contact() {
+    return <div>Contact</div>;
+}
 
 function App() {
-  return (
-      <>
-    <Header ></Header>
+  let routes = (
+      <Switch>
+        <Route path exact="/">
+            <Math />
+        </Route>
+        <Route path="/math">
+            <Math />
+        </Route>
+        <Route path="/programming">
+            <Programming />
+        </Route>
+        <Route path="/contact">
+            <Contact />
+        </Route>
+      </Switch>);
 
-      </>
+  return (
+
+      <Router>
+    <Header></Header>
+          {routes}
+      </Router>
+
   );
 }
 
