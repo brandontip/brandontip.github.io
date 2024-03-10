0bc0460dcfd1235c89d1828d863600a56998ff79
Brandon Tippings <tippingsbrandon@gmail.com>
2024-03-09 19:01:40 -0600

    Basic header made

---
+++ src/Components/Header.tsx
@@ -0,0 +1,13 @@
+import "./Header.css";
+
+function Header() {
+  return (
+    <header className="Header">
+        <div><strong>1.0</strong>Math</div>
+        <div>Programming</div>
+        <div>Contact</div>
+    </header>
+  );
+}
+
+export default Header;
